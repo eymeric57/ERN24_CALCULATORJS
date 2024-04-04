cd2645b724b4ac4eebc7ff0751e5dc1d330943e2
Format_Pro <eymeric57160@hotmail.fr>
2024-04-04 16:56:10 +0200

    add js calculator

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// ! fonction pour ajouter un éléments a l'écran
+
+function appendToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  let expression = document.getElementById("display").value;
+  console.log(expression);
+  let result = eval(expression);
+  console.log(result);
+  document.getElementById("display").value = result;
+}
